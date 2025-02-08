27b74e97bff1fdf1e43ad9152edf4a71054d857c
abhidumoli-debug <dipeshmaharaj9876@gmail.com>
2025-02-08 11:42:13 +0530

    Update info.py

--- info.py
+++ info.py
@@ -27,7 +27,7 @@ CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHAN
 #---------------------------------------------------------------
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://abhidumoli:0gMzQzk03ur2QHju@cluster0.ohqxe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'abhidumoli')
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 #----------- There will be channel id add in all these ---------
